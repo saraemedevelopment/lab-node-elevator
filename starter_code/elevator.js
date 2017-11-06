9d83be98c33e9be77eea90c6036c65afe2d11722
Sara <saraeme20@gmail.com>
2017-11-07 00:11:25 +0100

    iteration 4 in progress

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,9 +1,12 @@
 class Elevator {
   constructor() {
-    this.floor = 10;
+    this.floor = 0;
     this.MAXFLOOR = 10;
     this.direction = "up";
     this.requests = [];
+    this.requestedFloors = [];
+    this.waitingList = [];
+    this.passengers = [];
   }
 
   start() {
@@ -17,21 +20,37 @@ class Elevator {
   update() {
     this.log();
   }
-  _passengersEnter() {}
+  _passengersEnter() {
+
+    this.waitingList.forEach(elem => {
+        if (elem.originFloor === this.floor )
+          this.passengers.push(elem.name);
+          // this.waitingList.delete(person);
+          this.requestedFloors.push(elem.destinationFloor);
+          console.log(`${elem.name} has enter the elevator`);
+
+          });
+    }
+
   _passengersLeave() {}
+
   floorUp() {
     if (this.floor < this.MAXFLOOR) {
       this.floor++;
+      this._passengersEnter();
     }
   };
 
   floorDown() {
     if (this.floor > 0) {
       this.floor--;
+      this._passengersEnter(person);
+
     }
   }
   call(person) {
-    this.requests.push(person);
+    this.waitingList.push(person);
+    this.requestedFloors.push(person.originFloor);
   }
   log() {
     console.log(`Direction: ${this.direction} Floor: ${this.floor} `);
--- starter_code/index.js
+++ starter_code/index.js
@@ -2,10 +2,17 @@ const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
 let lift = new Elevator();
-let passenger1= new Person("Sara", 2, 3);
-
-lift.call(passenger1);
+let person1= new Person("Sara", 2, 3);
+let person2= new Person ("Celia", 4, 0);
 
 lift.log();
-
+lift.call(person1);
+lift.call(person2);
 console.log(lift.requests);
+console.log(lift.requestedFloors);
+lift.floorUp();
+lift.floorUp();
+lift.log();
+
+console.log(lift.passengers);
+console.log(lift.requestedFloors);
