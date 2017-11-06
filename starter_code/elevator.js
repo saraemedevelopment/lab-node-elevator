453fc5381d222d8cd3524e9b444a1ad9077368f1
Sara <saraeme20@gmail.com>
2017-11-06 20:20:03 +0100

    iteration 3 done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,41 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 10;
+    this.MAXFLOOR = 10;
+    this.direction = "up";
+    this.requests = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    let interval1 = setInterval(() => {
+      this.update();
+    }, 1000);
+  }
+  stop() {
+    clearInterval(interval1);
+  }
+  update() {
+    this.log();
+  }
+  _passengersEnter() {}
+  _passengersLeave() {}
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    }
+  };
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.floor--;
+    }
+  }
+  call(person) {
+    this.requests.push(person);
+  }
+  log() {
+    console.log(`Direction: ${this.direction} Floor: ${this.floor} `);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,11 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let lift = new Elevator();
+let passenger1= new Person("Sara", 2, 3);
+
+lift.call(passenger1);
+
+lift.log();
+
+console.log(lift.requests);
